158709eb7cfdabedc42d1ed01704928ffe374440
gatheluck <gatheluck+tech@gmail.com>
2020-03-22 14:27:22 +0900

    add plot function

--- misc/plot.py
+++ misc/plot.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 from misc.flag_holder import FlagHolder
 
 @click.command()
-@click.option('-c', '--csv_path', type=str, required=True)
+@click.option('-p', '--csv_path', type=str, required=True)
 @click.option('-x', type=str, required=True)
 @click.option('-y', type=str, default='')
 @click.option('-l', '--log_path', type=str, default='', help='path of log')
@@ -21,12 +21,49 @@ from misc.flag_holder import FlagHolder
 def main(**kwargs):
     plot(**kwargs)
 
+# def plot_from_csv(**kwargs):
+#     FLAGS = FlagHolder()
+#     FLAGS.initialize(**kwargs)
+#     FLAGS.summary()
+
+#     df = pd.read_csv(FLAGS.csv_path)
+
+#     fig = plt.figure()
+#     ax = fig.subplots()
+#     if FLAGS.y == '': raise ValueError('please specify "y"')
+#     sns.lineplot(x=FLAGS.x, y=FLAGS.y, ci="sd", data=df)
+
+#     if FLAGS.save:
+#         plt.close()
+#         if FLAGS.log_path == '': 
+#             raise ValueError('please specify "log_path"')
+#         os.makedirs(os.path.dirname(FLAGS.log_path), exist_ok=True)
+#         fig.savefig(FLAGS.log_path)
+#     else:
+#         plt.show()
+
 def plot(**kwargs):
+    # parse keyword arugments
+    required_kwargs = 'x y log_path save'.split()
+    for required_kwarg in required_kwargs:
+        if required_kwarg not in kwargs.keys(): 
+            raise ValueError('invalid args')
+
+    has_dataframe = 'dataframe' in kwargs.keys()
+    has_csv_path  = 'csv_path'  in kwargs.keys()
+
+    if (has_dataframe and has_csv_path) or (not has_dataframe and  not has_csv_path):
+        raise ValueError('invalid args')
+
+    # set flag
     FLAGS = FlagHolder()
     FLAGS.initialize(**kwargs)
     FLAGS.summary()
 
-    df = pd.read_csv(FLAGS.csv_path)
+    if has_csv_path:
+        df = pd.read_csv(FLAGS.csv_path)
+    else:
+        df = FLAGS.dataframe
 
     fig = plt.figure()
     ax = fig.subplots()
