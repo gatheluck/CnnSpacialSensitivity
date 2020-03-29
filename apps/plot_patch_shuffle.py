367c6278266e657aad94634aefb857d764347322
gatheluck <gatheluck+tech@gmail.com>
2020-03-30 06:28:02 +0900

    ignore redundant args

--- apps/plot_patch_shuffle.py
+++ apps/plot_patch_shuffle.py
@@ -18,21 +18,18 @@ from misc.plot import plot
 @click.command()
 # target
 @click.option('-t', '--target_dir', type=str, required=True)
-@click.option('-x', type=str, required=True)
-@click.option('-y', type=str, default='')
-@click.option('-h', '--hue', type=str, default=None, help='name of grouping variable that will produce lines with different colors')
 @click.option('-l', '--log_path', type=str, default='', help='path of log')
 @click.option('-s', '--save', is_flag=True, default=False, help='save results')
-
 def main(**kwargs):
     plot_patch_shuffle(**kwargs)
+
 
 def plot_patch_shuffle(**kwargs):
     FLAGS = FlagHolder()
     FLAGS.initialize(**kwargs)
 
     target_path = os.path.join(FLAGS.target_dir, '**/*.csv')
-    csv_paths   = sorted(glob.glob(target_path, recursive=True), key=lambda x: os.path.basename(x))
+    csv_paths = sorted(glob.glob(target_path, recursive=True), key=lambda x: os.path.basename(x))
 
     df = None
     for csv_path in csv_paths:
@@ -44,8 +41,9 @@ def plot_patch_shuffle(**kwargs):
             df = new_df
         else:
             df = pd.concat([df, new_df], axis=0)
-      
+
     plot(dataframe=df, x='num_devide', y='accuracy', hue='legend', log_path=os.path.join(FLAGS.log_path, 'plot_patch_shuffle.png'), save=FLAGS.save)
+
 
 if __name__ == '__main__':
     main()
